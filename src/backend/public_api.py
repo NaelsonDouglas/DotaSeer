12b0dbdb8ff7f5be5d022be4b774db2e8ae234fa
naelsondouglas <naelson17@gmail.com>
2020-07-26 22:24:07 -0300

    Added missing files

---
+++ src/backend/public_api.py
@@ -0,0 +1,29 @@
+import flask
+from flask import request, jsonify
+from knn import Knn
+import logging
+from configure_logging import configure_logging
+from flask_cors import CORS
+configure_logging('public_api.py')
+
+class PublicApi():
+        def __init__(self):
+                self.app = flask.Flask(__name__)
+                CORS(self.app)
+                self.app.config["DEBUG"] = True
+                self.knn = Knn(7)
+
+        def start(self):
+                @self.app.route('/api/predict', methods=['POST'])
+                def api_id():    
+                        logging.info(request.args)
+                        if 'radiant_score' in request.args and 'dire_score' in request.args and 'duration' in request.args:
+                                radiant_score = int(request.args['radiant_score'])
+                                dire_score = int(request.args['dire_score'])
+                                duration = int(request.args['duration'])
+                                result = self.knn.predict([[radiant_score,dire_score,duration]])
+                                logging.info(result[0])
+                                return str('{"result": '+str(result[0])+'}')
+                        else:
+                                return "Missed a field"
+                self.app.run(host='0.0.0.0', port=8080)
