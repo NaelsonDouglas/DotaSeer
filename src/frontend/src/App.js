954e2811d58ae765ba97b7c63d03ab9b9b7c9d32
naelsondouglas <naelson17@gmail.com>
2020-07-26 23:20:18 -0300

    fixed some styling

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import Result from './Result';
 import axios from 'axios';
-
-
-
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = { radiant_score: 0, dire_score: 0, duration:0, result:-1 };
+    this.state = { radiant_score: 0, dire_score: 0, duration:0, result:'*' };
   } 
  
   radiantHandleChange = event => {
@@ -41,89 +36,58 @@ class App extends React.Component {
     axios.post(url, null,{params:match})
       .then(res => {
         console.log(res);
-        console.log(res.data);
+        console.log(res.data);        
+        this.setState({result:res.data.result})
       })
-
-    console.log("aaaaaaaaaaaa");
-    this.setState({result:20})
-
   };   
  
   render() {
     return (
-      <React.Fragment>
-        <form onSubmit={this.submitHandle}>
-          <label htmlFor="radiant_score">radiant_score</label>
+      
+      <React.Fragment >        
+        <form onSubmit={this.submitHandle} style={{textAlign:"center", display:"block",alignContent:"center", backgroundColor:"black"}}>
+          <label htmlFor="radiant_score" style={{color:"green"}}>radiant_score</label>
           <input
             type="number"
             name="radiant_score"            
             onChange={this.radiantHandleChange}
           />
-
+          <div></div>
+          
+          <label htmlFor="dire_score" style={{color:"red"}}>dire_score</label>
           <input
             type="number"
             name="dire_score"            
             onChange={this.direHandleChange}
           />
-
+          <div></div>
+          <label htmlFor="duration" style={{color:"white"}}>duration</label>
           <input
             type="number"
             name="duration"            
             onChange={this.durationHandleChange}
           />
+          <div></div>
           <input type="submit" value="Predict" style={{ marginLeft: "auto" }}/>
+          <h3 style={{color:"white"}}>Result: {this.state.result}</h3>        
         </form> 
-        <h3>Radiant score: {this.state.radiant_score}</h3>
+          
+        {/* <h3>Radiant score: {this.state.radiant_score}</h3>
         <h3>Radiant score: {this.state.dire_score}</h3>
-        <h3>Duration: {this.state.duration}</h3>
-        <h3>Result: {this.state.result}</h3>
-      </React.Fragment>
+        <h3>Duration: {this.state.duration}</h3> */}
+        
+      </React.Fragment>      
     );
   }
  }
  export default App;
 
-
-// import React from 'react';
-// import { useForm } from 'react-hook-form';
-// import Result from './Result';
-// import axios from 'axios';
-
-
-
-// export default function App() {
-//   var dt = 10000000;  
-//   const { register, handleSubmit } = useForm();
-  
-//   const onSubmit = data => {
-//     console.log(data)    
-//     const url = 'http://127.0.0.1:8080/api/predict?radiant_score=1&dire_score=10&duration=5000'
-//     const params = {'radiant_score':'1','dire_score':'2','duration':'3'}
-//     axios.post(url).then(res => {
-//         console.log(res.data);
-//         //this.dt=res.data.result
-//         this.setState({result:res.data.result});
-//     })
-//   };   
- 
-
-//   return (    
-//     <div style={{ display: "flex" }}>
-//       <div>
-//         <form onSubmit={handleSubmit(onSubmit)} >      
-//             Radiant score:
-//             <input name="radiant_score" defaultValue="0" ref={register} style={{ marginLeft: "right" }}/>
-//             <div></div>
-//             Dire score:
-//             <input name="dire_score" defaultValue="0" ref={register} style={{ marginLeft: "right"}}/>
-//             <div></div>
-//             Duration:
-//             <input name="duration" defaultValue="0" ref={register} style={{ marginLeft: "right" }}/>
-//             <div></div>
-//             <input type="submit" value="Predict" style={{ marginLeft: "auto" }}/>
-//         </form>        
-//         <Result result={dt}></Result>
-//       </div>
-//     </div>
-//   );
-// }
+ const StyledForm = `
+ width: 100%;
+ max-width: 700px;
+ padding: 40px;
+ background-color: #fff;
+ border-radius: 10px;
+ box-sizing: border-box;
+ box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+`;
