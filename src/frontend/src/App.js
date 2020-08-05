3dc0df8fb92b3180a5f50962b3efa4c759e3aabf
naelsondouglas <naelson17@gmail.com>
2020-08-05 16:29:52 -0300

    Added some explaining to the problem into the screen

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -50,9 +50,15 @@ class App extends React.Component {
   render() {
     return (
       
-      <React.Fragment >                
+      <React.Fragment >                        
       <form onSubmit={this.submitHandle} style={{textAlign:"center", display:"block",alignContent:"center", backgroundColor:"black"}}>
-          
+      <p>
+            <label style={{color:"white"}}>Dota is a 5v5 online game where two teams, Radiant(green) and Dire(red) fight one against the other to destroy the rival building  named 'ancient'.</label>            
+            <p><label style={{color:"white"}}>Each team must defend their ancient, therefore, they are called Defenders of the Ancients(DOTA)</label></p>
+            <p><label style={{color:"white"}}>When a player from the Dire team dies, Radiant scores one point and the same happens to Dire when a member of the Radiant team dies.</label></p>
+            <p><label style={{color:"white"}}>It's quite obvious to say a team with bigger score has more chances to win, but there's a popular say among Dota players where it's said 'score doesn't matter on long matches'. That's because on long matches, each team is already playing on the highest level, so killing more or less  doesnt  inflict  such big difference.</label></p>
+            <p><label style={{color:"white"}}>This predictor uses 3 variables Radiant Score, Team Score and Match duration(s) to predict  which team wins  given the situation.</label></p>
+      </p>
           <input
             type="number"
             placeholder="Radiant  Score"            
@@ -99,7 +105,7 @@ class App extends React.Component {
                       alignItems: 'center',
                       justifyContent: 'center',
                       }}> 
-          <img alignContent="center" style={{width:"100%"}} alt="Data plot" src={require('./plot/plot.png')}/>
+          <img alignContent="center" style={{width:"80%"}} alt="Data plot" src={require('./plot/plot.png')}/>
         </div>
       </React.Fragment>      
     );
